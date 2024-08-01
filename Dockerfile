0a0c5ade72329f58d65d90cd0b7b44f7c1a4becd
OksanaFedotova <oxana-fedotova@yandex.ru>
2024-08-01 22:30:23 +0400

    feat: minimize docker container

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,24 @@
-FROM node:18 AS build
-
+# Stage 1: Base image
+FROM node:20-alpine AS base
 WORKDIR /app
 
+# Stage 2: Install dependencies
+FROM base AS dependencies
 COPY package*.json ./
-RUN npm install
+RUN npm install && npm cache clean --force
 
+# Stage 3: Build application
+FROM dependencies AS build
 COPY . .
+RUN npm run build
+
+# Stage 4: Final image
+FROM node:20-alpine AS final
+WORKDIR /app
+COPY --from=dependencies /app/node_modules ./node_modules
+COPY --from=build /app/dist ./dist
+
+USER node
 
 ENV PORT 4000
 EXPOSE 4000
---
+++ src/mainLambda.ts
@@ -0,0 +1,50 @@
+import { NestFactory } from '@nestjs/core';
+import { configure as serverlessExpress } from '@codegenie/serverless-express';
+import helmet from 'helmet';
+import { AppModule } from './app.module';
+import { Callback, Context, Handler } from 'aws-lambda';
+
+let server: Handler;
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: (req, callback) => callback(null, true),
+  });
+  app.use(helmet());
+  await app.init(); // Инициализация приложения NestJS
+
+  const expressApp = app.getHttpAdapter().getInstance(); // Получение экземпляра Express приложения
+
+  if (process.env.NODE_ENV === 'production') {
+    // Код для AWS Lambda
+    return serverlessExpress({ app: expressApp }); // Возвращ ение серверного обработчика для Lambda
+  } else {
+    // Код для локального запуска
+    const port = process.env.PORT || 4000;
+    expressApp.listen(port, '0.0.0.0', () => {
+      console.log(`Server is running on http://localhost:${port}`);
+    });
+    return null; // Для локального запуска не требуется возвращать серверный обработчик
+  }
+}
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  server = server ?? (await bootstrap()); // Инициализация сервера при первом вызове
+  if (server) {
+    return server(event, context, callback); // Обработка запроса для AWS Lambda
+  }
+  return null; // Локальная среда не требует обработки
+};
+
+// Запуск сервера локально
+if (require.main === module) {
+  bootstrap().catch((err) => {
+    console.error('Error bootstrapping the application', err);
+    process.exit(1);
+  });
+}
